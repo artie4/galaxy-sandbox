1ba099bca826d9418787c539969d54afd149fe32
arthur <gaartur4@gmail.com>
2025-01-08 12:31:47 +0300

    up java to 17, spring boot 3.3.4, kotlin 1.9.20

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,25 +1,25 @@
 object SpringCloud {
-    const val VERSION = "2020.0.1"
+    const val VERSION = "2023.0.1"
 }
 
 object SpringBoot {
-    const val VERSION = "2.4.3"
+    const val VERSION = "3.3.4"
 }
 
 object Spring {
-    const val DEPENDENCY_MANAGEMENT_VERSION = "1.0.11.RELEASE"
+    const val DEPENDENCY_MANAGEMENT_VERSION = "1.1.0"
 }
 
 object Kotlin {
-    const val VERSION = "1.4.30"
+    const val VERSION = "1.9.20"
 }
 
 object TestContainers {
-    const val VERSION = "1.15.1"
+    const val VERSION = "1.18.1"
 }
 
 object MicroUtilsKotlin {
-    const val LOGGING_VERSION = "1.12.0"
+    const val LOGGING_VERSION = "6.0.9"
 }
 
 object Database {
