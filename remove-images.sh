9181ee8e11a999bf5978e88590c796bd81878205
arthur <gaartur4@gmail.com>
2025-01-16 12:48:37 +0300

    add data-tier module in compose

--- remove-images.sh
+++ remove-images.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-eureka_image=galaxy-sandbox/eureka-server
-generator_image=galaxy-sandbox/event-generator
-consumer_image=galaxy-sandbox/event-consumer
+eureka_image=localhost/galaxy-sandbox/eureka-server
+generator_image=localhost/galaxy-sandbox/event-generator
+consumer_image=localhost/galaxy-sandbox/event-consumer
+datatier_image=localhost/galaxy-sandbox/data-tier
 
-podman rmi $eureka_image $generator_image $consumer_image
+podman rmi $eureka_image $generator_image $consumer_image $datatier_image
