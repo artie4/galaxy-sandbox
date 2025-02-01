d899b327c524b7c1938567763273d1d4f388eb75
arthur <gaartur4@gmail.com>
2025-02-01 09:48:28 +0300

    add order management app in docker compose

--- remove-containers.sh
+++ remove-containers.sh
@@ -4,5 +4,6 @@ eureka_container=galaxysandbox_discovery_1
 consumer_container=galaxysandbox_event-consumer_1
 generator_container=galaxysandbox_event-generator_1
 datatier_container=galaxysandbox_data-tier_1
+ordermanagement_container=galaxysandbox_order-management_1
 
-podman rm -f $eureka_container $consumer_container $generator_container $datatier_container
+podman rm -f $eureka_container $consumer_container $generator_container $datatier_container $ordermanagement_container
--- start-kafka-broker.sh
+++ start-kafka-broker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-kafka_dir=~/kafka_2.13-2.7.0
+kafka_dir=~/kafka/kafka_2.13-3.9.0
 
 ${kafka_dir}/bin/zookeeper-server-start.sh ${kafka_dir}/config/zookeeper.properties &
 
